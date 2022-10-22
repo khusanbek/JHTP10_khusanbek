9d7425fcab037753857005199899bc0c31853ae1
Khusanbek Gafurov <x.gafurov@gmail.com>
2022-10-22 17:09:37 +0500

    Update Buffer.java

--- ch23/23.7/Buffer.java
+++ ch23/23.7/Buffer.java
@@ -8,3 +8,15 @@ public interface Buffer
 	// return int value from Buffer
 	public int blockingGet() throws InterruptedException;
 } // end interface Buffer
+
+/*
+ * Khusanbek Gafurov Practical Codes from JHTP10.
+ * These hand written codes are written for educational purposes only.
+ * 
+ * The JHTP10 is an amazing book, but, I found few mistakes in the source
+ * code of the book. So, due to this, decided to hand code myself and 
+ * upload here as a reference.
+ *
+ * Khusanbek Gafurov 2022
+ *
+**/
