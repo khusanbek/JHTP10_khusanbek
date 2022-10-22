a80674ba0b2bc80da769c62e23e351cb34ab98f5
Khusanbek Gafurov <x.gafurov@gmail.com>
2022-10-22 17:09:15 +0500

    Update Consumer.java

--- ch23/23.7/Consumer.java
+++ ch23/23.7/Consumer.java
@@ -37,3 +37,15 @@ public class Consumer implements Runnable
 			"Consumer read values totaling", sum, "Terminating Consumer");		
 	}
 } // end class Consumer
+
+/*
+ * Khusanbek Gafurov Practical Codes from JHTP10.
+ * These hand written codes are written for educational purposes only.
+ * 
+ * The JHTP10 is an amazing book, but, I found few mistakes in the source
+ * code of the book. So, due to this, decided to hand code myself and 
+ * upload here as a reference.
+ *
+ * Khusanbek Gafurov 2022
+ *
+**/
