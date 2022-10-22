9a7a5f2c60d0bb7503f3ce19e647b948c2d7b7f8
Khusanbek Gafurov <x.gafurov@gmail.com>
2022-10-22 17:07:51 +0500

    Add files via upload

---
+++ ch23/23.7/SharedBufferTest2.java
@@ -0,0 +1,27 @@
+// Fig.23.17: SharedBufferTest2.java
+// Two threads correctly manipulating a synchronized buffer.
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SharedBufferTest2
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		// create a newCachedThreadPool
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		
+		// create SynchronizedBuffer to store ints
+		Buffer sharedLocation = new SynchronizedBuffer();
+		
+		System.out.printf("%-40s%s\t\t%s%n%-40s%s%n%n", "Operation",
+			"Buffer", "Occupied", "---------", "------\t\t--------");
+			
+		// execute the Producer and Consumer tasks
+		executorService.execute(new Producer(sharedLocation));
+		executorService.execute(new Consumer(sharedLocation));
+		
+		executorService.shutdown();
+		executorService.awaitTermination(1, TimeUnit.MINUTES);
+	}
+} // end class SharedBufferTest2
